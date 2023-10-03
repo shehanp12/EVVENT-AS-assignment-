06f5604e58167867967bb9cb82c7dfb5d3f92b69
Shehan Prasanna <shehanp068@gmail.com>
2023-10-03 19:53:42 +0200

    navigation bar and separator

--- components/main-nav.tsx
+++ components/main-nav.tsx
@@ -12,11 +12,8 @@ interface MainNavProps {
 
 export function MainNav({ items }: MainNavProps) {
   return (
-    <div className="flex gap-6 md:gap-10">
-      <Link href="/" className="flex items-center space-x-2">
-        <Icons.logo className="h-6 w-6" />
-        <span className="inline-block font-bold">{siteConfig.name}</span>
-      </Link>
+   
+    <div className="flex gap-6 md:gap-10 ">
       {items?.length ? (
         <nav className="flex gap-6">
           {items?.map(
@@ -37,5 +34,6 @@ export function MainNav({ items }: MainNavProps) {
         </nav>
       ) : null}
     </div>
+    
   )
 }
